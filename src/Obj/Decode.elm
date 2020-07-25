3f585dd988cfe132e9ed37a0d3bf9950f03a36c1
Andrey Kuzmin <andrey.kuzmin@soundcloud.com>
2020-07-25 11:18:32 +0200

    Added some docs

--- src/Obj/Decode.elm
+++ src/Obj/Decode.elm
@@ -1,21 +1,40 @@
 module Obj.Decode exposing
-    ( Decoder
-    , Filter
-    , ObjCoordinates
+    ( Decoder, triangles, faces, texturedTriangles, texturedFaces
+    , Filter, object, group, defaultGroup, material
     , decodeString
-    , defaultGroup
-    , faces
-    , fail
-    , group
-    , map2
-    , material
-    , object
-    , oneOf
-    , succeed
-    , texturedFaces
-    , texturedTriangles
-    , triangles
+    , map, map2, map3, map4, map5
+    , oneOf, fail, succeed
+    , ObjCoordinates
     )
+
+{-|
+
+
+# Primitives
+
+@docs Decoder, triangles, faces, texturedTriangles, texturedFaces
+
+
+# Filtering
+
+@docs Filter, object, group, defaultGroup, material
+
+
+# Run Decoders
+
+@docs decodeString
+
+
+# Mapping
+
+@docs map, map2, map3, map4, map5
+
+
+# Advanced Decoding
+
+@docs oneOf, fail, succeed
+
+-}
 
 import Array exposing (Array)
 import Length exposing (Length, Meters)
@@ -23,6 +42,176 @@ import Point3d exposing (Point3d)
 import Quantity exposing (Unitless)
 import TriangularMesh exposing (TriangularMesh)
 import Vector3d exposing (Vector3d)
+
+
+{-| A value that knows how to decode information from
+[the OBJ file format](https://en.wikipedia.org/wiki/Wavefront_.obj_file)
+-}
+type Decoder a
+    = Decoder (ParsedFile -> Result String a)
+
+
+{-| Decode just the plain positions. Use with `Scene3d.Mesh.indexedTriangles` and `Scene3d.Mesh.indexedFaces` from elm-3d-scene.
+-}
+triangles : List Filter -> Decoder (TriangularMesh (Point3d Meters ObjCoordinates))
+triangles filters =
+    fail "Implement triangles"
+
+
+{-| Decode positions and normals. Use with `Scene3d.Mesh.indexedFaces`.
+-}
+faces : List Filter -> Decoder (TriangularMesh { position : Point3d Meters ObjCoordinates, normal : Vector3d Unitless ObjCoordinates })
+faces filters =
+    fail "Implement faces"
+
+
+{-| Decode positions and [UV](https://learnopengl.com/Getting-started/Textures) (texture) coordinates.
+Use with `Scene3d.Mesh.texturedTriangles` or `Scene3d.Mesh.texturedFacets`.
+-}
+texturedTriangles : List Filter -> Decoder (TriangularMesh { position : Point3d Meters ObjCoordinates, uv : ( Float, Float ) })
+texturedTriangles filters =
+    fail "Implement texturedTriangles"
+
+
+{-| Decode positions, UV and normals. Use with `Scene3d.Mesh.texturedFaces`.
+-}
+texturedFaces : List Filter -> Decoder (TriangularMesh { position : Point3d Meters ObjCoordinates, normal : Vector3d Unitless ObjCoordinates, uv : ( Float, Float ) })
+texturedFaces filters =
+    Decoder
+        (\parsedFile ->
+            case List.filter (Tuple.first >> matches filters) parsedFile.faces of
+                [] ->
+                    Err "Not found"
+
+                filteredFaces ->
+                    collectTexturedFaces parsedFile filteredFaces []
+        )
+
+
+{-| All the primitive decodes take a list of filters. This lets you select only the subset of polygons from the OBJ file.
+All filters in the list are joined with the `and`.
+-}
+type Filter
+    = Group String
+    | Material String
+    | Object String
+
+
+{-| Filter polygons that belong to a certain group.
+-}
+group : String -> Filter
+group =
+    Group
+
+
+{-| Filter by default group. This group has a special meaning,
+all polygons are assigned to it if a group is not specified.
+
+    defaultGroup =
+        group "default"
+
+-}
+defaultGroup : Filter
+defaultGroup =
+    Group "default"
+
+
+{-| Filter by object. You may store multiple objects in the same OBJ file. Like a car base and car wheels.
+-}
+object : String -> Filter
+object =
+    Object
+
+
+{-| Filter by material name.
+-}
+material : String -> Filter
+material =
+    Material
+
+
+{-| Run the decoder on the string. Takes a function, that knows
+how to convert float coordinates into physical units.
+
+    decodeString Length.centimeters (texturedFaces []) string == Ok (TriangularMesh ...)
+    decodeString Length.centimeters (texturedFaces []) string == Err ...
+
+-}
+decodeString : (Float -> Length) -> Decoder a -> String -> Result String a
+decodeString units (Decoder decode) content =
+    content
+        |> parse units
+        |> Result.andThen decode
+
+
+{-| Transform the decoder. May be useful in case need to post process the mesh data.
+-}
+map : (a -> b) -> Decoder a -> Decoder b
+map fn (Decoder decoder) =
+    Decoder (\parsedFile -> Result.map fn (decoder parsedFile))
+
+
+{-| Join the result from two decoders. This lets you extract parts of the same OBJ file into separate meshes.
+
+    carDecoder =
+        map2 (\wheels base -> { wheels = wheels, base = base }) wheelsDecoder baseDecoder
+
+-}
+map2 : (a -> b -> c) -> Decoder a -> Decoder b -> Decoder c
+map2 fn (Decoder decoderA) (Decoder decoderB) =
+    Decoder (\parsedFile -> Result.map2 fn (decoderA parsedFile) (decoderB parsedFile))
+
+
+{-| -}
+map3 : (a -> b -> c -> d) -> Decoder a -> Decoder b -> Decoder c -> Decoder d
+map3 fn (Decoder decoderA) (Decoder decoderB) (Decoder decoderC) =
+    Decoder (\parsedFile -> Result.map3 fn (decoderA parsedFile) (decoderB parsedFile) (decoderC parsedFile))
+
+
+{-| -}
+map4 : (a -> b -> c -> d -> e) -> Decoder a -> Decoder b -> Decoder c -> Decoder d -> Decoder e
+map4 fn (Decoder decoderA) (Decoder decoderB) (Decoder decoderC) (Decoder decoderD) =
+    Decoder (\parsedFile -> Result.map4 fn (decoderA parsedFile) (decoderB parsedFile) (decoderC parsedFile) (decoderD parsedFile))
+
+
+{-| -}
+map5 : (a -> b -> c -> d -> e -> f) -> Decoder a -> Decoder b -> Decoder c -> Decoder d -> Decoder e -> Decoder f
+map5 fn (Decoder decoderA) (Decoder decoderB) (Decoder decoderC) (Decoder decoderD) (Decoder decoderE) =
+    Decoder (\parsedFile -> Result.map5 fn (decoderA parsedFile) (decoderB parsedFile) (decoderC parsedFile) (decoderD parsedFile) (decoderE parsedFile))
+
+
+{-| Try a bunch of different decoders. You will get the result from the first one that succeeds.
+-}
+oneOf : List (Decoder a) -> Decoder a
+oneOf decoders =
+    Decoder (\parsedFile -> oneOfHelp parsedFile decoders)
+
+
+{-| A decoder that always succeeds with the result. May be useful in combination with `oneOf` to
+provide a placeholder mesh if decoding fails.
+-}
+succeed : a -> Decoder a
+succeed mesh =
+    Decoder (\_ -> Result.Ok mesh)
+
+
+{-| A decoder that always fails with a given error message.
+Use it in case you need custom error messages.
+-}
+fail : String -> Decoder a
+fail error =
+    Decoder (\_ -> Result.Err error)
+
+
+
+-- Internals
+
+
+type alias TexturedFace =
+    { position : Point3d Meters ObjCoordinates
+    , normal : Vector3d Unitless ObjCoordinates
+    , uv : ( Float, Float )
+    }
 
 
 type ObjCoordinates
@@ -44,50 +233,20 @@ type alias ParsedFile =
     }
 
 
-type Decoder a
-    = Decoder (ParsedFile -> Result String a)
+type PropertyType
+    = GroupsProperty (List String)
+    | ObjectProperty String
+    | MaterialProperty String
 
 
-decodeString : (Float -> Length) -> Decoder a -> String -> Result String a
-decodeString units (Decoder decode) content =
-    content
-        |> parse units
-        |> Result.andThen decode
-
-
-triangles : List Filter -> Decoder (TriangularMesh (Point3d Meters ObjCoordinates))
-triangles filters =
-    fail "Implement triangles"
-
-
-faces : List Filter -> Decoder (TriangularMesh { position : Point3d Meters ObjCoordinates, normal : Vector3d Unitless ObjCoordinates })
-faces filters =
-    fail "Implement faces"
-
-
-texturedTriangles : List Filter -> Decoder (TriangularMesh { position : Point3d Meters ObjCoordinates, uv : ( Float, Float ) })
-texturedTriangles filters =
-    fail "Implement texturedTriangles"
-
-
-texturedFaces : List Filter -> Decoder (TriangularMesh { position : Point3d Meters ObjCoordinates, normal : Vector3d Unitless ObjCoordinates, uv : ( Float, Float ) })
-texturedFaces filters =
-    Decoder
-        (\parsedFile ->
-            case List.filter (Tuple.first >> matches filters) parsedFile.faces of
-                [] ->
-                    Err "Not found"
-
-                filteredFaces ->
-                    collectTexturedFaces parsedFile filteredFaces []
-        )
-
-
-type alias TexturedFace =
-    { position : Point3d Meters ObjCoordinates
-    , normal : Vector3d Unitless ObjCoordinates
-    , uv : ( Float, Float )
-    }
+type Line
+    = Property PropertyType
+    | PositionData (Point3d Meters ObjCoordinates)
+    | NormalData (Vector3d Unitless ObjCoordinates)
+    | UvData ( Float, Float )
+    | FaceIndices (List (List (Maybe Int)))
+    | Error String
+    | Skip
 
 
 collectTexturedFaces : ParsedFile -> List ( GroupProperties, List (List (List (Maybe Int))) ) -> List ( TexturedFace, TexturedFace, TexturedFace ) -> Result String (TriangularMesh TexturedFace)
@@ -161,42 +320,6 @@ addTexturedFacesPolygons parsedFile firstVertex vertices groupIndices polygons =
             Err "Missing indices"
 
 
-type Filter
-    = Group String
-    | Material String
-    | Object String
-
-
-group : String -> Filter
-group =
-    Group
-
-
-defaultGroup : Filter
-defaultGroup =
-    Group "default"
-
-
-object : String -> Filter
-object =
-    Object
-
-
-material : String -> Filter
-material =
-    Material
-
-
-map2 : (a -> b -> c) -> Decoder a -> Decoder b -> Decoder c
-map2 fn (Decoder decoderA) (Decoder decoderB) =
-    Decoder (\parsedFile -> Result.map2 fn (decoderA parsedFile) (decoderB parsedFile))
-
-
-oneOf : List (Decoder a) -> Decoder a
-oneOf decoders =
-    Decoder (\parsedFile -> oneOfHelp parsedFile decoders)
-
-
 oneOfHelp : ParsedFile -> List (Decoder a) -> Result String a
 oneOfHelp parsedFile decoders =
     case decoders of
@@ -210,36 +333,6 @@ oneOfHelp parsedFile decoders =
 
                 Err _ ->
                     oneOfHelp parsedFile remainingDecoders
-
-
-succeed : a -> Decoder a
-succeed mesh =
-    Decoder (\_ -> Result.Ok mesh)
-
-
-fail : String -> Decoder a
-fail error =
-    Decoder (\_ -> Result.Err error)
-
-
-
--- Internals
-
-
-type PropertyType
-    = GroupsProperty (List String)
-    | ObjectProperty String
-    | MaterialProperty String
-
-
-type Line
-    = Property PropertyType
-    | PositionData (Point3d Meters ObjCoordinates)
-    | NormalData (Vector3d Unitless ObjCoordinates)
-    | UvData ( Float, Float )
-    | FaceIndices (List (List (Maybe Int)))
-    | Error String
-    | Skip
 
 
 parse : (Float -> Length) -> String -> Result String ParsedFile
